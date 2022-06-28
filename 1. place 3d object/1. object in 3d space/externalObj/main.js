05859317bea59808ba59cd39ffb8f35e5316494e
UddiptaG <uddipta.gogoi@slow.studio>
2022-06-28 17:59:40 +0530

    added linear movement

--- 1. place 3d object/1. object in 3d space/externalObj/main.js
+++ 1. place 3d object/1. object in 3d space/externalObj/main.js
@@ -3,61 +3,66 @@ const scene=new THREE.Scene();
 const gui=new dat.GUI();
 
 
+var model;
+
 //calling the declared functions
 var plane=getPlane();
-var ambLight=getAmbientLight(0.35);
-var pointLight1=getPointLight(0.5);
-var pointLight2=getPointLight(0.1);
+var ambLight=getAmbientLight(0.4);
+var hemiLight=getHemiLight(0.4);
+var pointLight1=getPointLight(0.2);
+var spotLight1=getSpotLight(0.5);
+var gltfObj1=getGLTFLoader('./assets/roun.glb',-0.5,0,1);        //here we are passing the obj location and positions as the parameter
 
 //adding elements to the scene
 scene.add(plane);
-scene.add(ambLight);
-scene.add(pointLight1);
-scene.add(pointLight2);
+scene.add(hemiLight);
+scene.add(spotLight1);
 
 //rotating the plan on the x axis to use it as a floor
 plane.rotateX( - Math.PI / 2);
 
 //adding a perspective camera to the scene
 var camera=new THREE.PerspectiveCamera(
-    45,                                         //FOV
+    55,                                         //FOV
     window.innerWidth / window.innerHeight,     //aspect ration
     0.1,                                        //near
     1000                                        //far
 );
 
 //set camera positions
-camera.position.set(-3,5,3);
-pointLight1.position.set(5,12,5);
-pointLight2.position.set(-10,20,-5);
-
-
+camera.position.set(-3,3,4);
+spotLight1.position.set(30,20,25);
 
 //add the light source as a child of the camera to make the camera as the soure of the light
-//camera.add(pointLight);
+camera.add(pointLight1);
 
 //we need to add the camera to the scene as we created a child of it
-//scene.add(camera)
+scene.add(camera)
 
-//adding the GUI controls for the point light
-const pointLightGUI=gui.addFolder('point light intensity')
-//adding the GUI controls
-pointLightGUI.add(pointLight1, 'intensity').min(0).max(10).step(0.01);
+//adding the GUI controls for each element
+const spotLightGUI=gui.addFolder('SpotLight Controls')
+spotLightGUI.add(spotLight1, 'intensity').min(0).max(10).step(0.01);
+spotLightGUI.add(spotLight1.position, 'x').min(-50).max(100).step(0.01);
+spotLightGUI.add(spotLight1.position, 'y').min(-50).max(100).step(0.01);
+spotLightGUI.add(spotLight1.position, 'z').min(-50).max(100).step(0.01);
 
 //setting up the renderer
 const renderer=new THREE.WebGLRenderer({
     alpha:true,
-    antialias:true
+    antialias:true,
+    depth: true
 });   //creating an instance of the renderer
 
 renderer.shadowMap.enabled = true;                          //enabling shadow in render
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;           //adding shadow type as soft shadow
 renderer.setSize( window.innerWidth, window.innerHeight);   //setting up the size of the renderer
+renderer.setClearColor(new THREE.Color('#ffffff'),0.45)
 document.body.appendChild( renderer.domElement);
 
 
 //setting up Orbit Controls
 controls = new THREE.OrbitControls(camera,renderer.domElement);
+//controls.enableRotate=false;
 
 //function to get an Ambient Light-------------------
 function getAmbientLight(intensity){
@@ -66,17 +71,35 @@ function getAmbientLight(intensity){
     return light;
 }
 
+//function to add a hemi light
+function getHemiLight(intensity){
+    const light=new THREE.HemisphereLight(0xffffee,0xffffee, intensity)
+    return light;
+}
+
 //function to add a PointLight-----------------------
 function getPointLight(intensity){
     const light = new THREE.PointLight(0xffffff, intensity);
     light.castShadow=true;
+    // light.shadow.radius = 5;
     return light;
 }
 
 
+//function to add a spotlight
+function getSpotLight(intensity){
+    const light=new THREE.SpotLight(0xffffff,intensity);
+    light.castShadow=true;
+    
+    light.shadow.bias= -0.00001;
+    light.shadow.mapSize.width=1024*2;
+    light.shadow.mapSize.height=1024*2;
+    return light;
+}
+
 //function to add a plan-------------------------------
 function getPlane(){
-    const geometry=new THREE.PlaneGeometry(400,400);
+    const geometry=new THREE.PlaneGeometry(30,30);
     const material=new THREE.MeshPhongMaterial({
         color: 0xffffff,
         side: THREE.DoubleSide
@@ -85,34 +108,44 @@ function getPlane(){
     mesh.receiveShadow = true;      //set this to true to allow the object to recieve the shadow
     return mesh;
 }
-//function to call the GLTF Loader
-const loader=new THREE.GLTFLoader();
-loader.load( './assets/roun.glb', function ( gltf ) {
+
+//function to call the GLTF Loader----the obj location and positions are passed as parameters in the function call
+function getGLTFLoader(assetLocation,positionX,positionY,positionZ){
+    const loader=new THREE.GLTFLoader();
+loader.load( assetLocation, function ( gltf ) {
     model=gltf.scene;
 
-    const newMaterial = new THREE.MeshPhongMaterial({color: 0xffcc00   
+    const newMaterial = new THREE.MeshStandardMaterial({
+                                    color: 0xffcc00,
+                                   // wireframe: true,
+                                    roughness: 0,
+                                    emissive: 0x000000,
+                                    
                         });
 						model.traverse((o) => {
 						if (o.isMesh) o.material = newMaterial;
 						}); 
 
+                        //model.wireframe=true    
                         model.castShadow = true;
                         model.traverse(function (node) {
                           if (node.isMesh) {
                             node.castShadow = true;
-                            //node.receiveShadow = true;
+                            node.receiveShadow = true;
                           }
                         });
+    model.position.set(positionX,positionY,positionZ);                        
 	scene.add(model);
 }, undefined, function ( error ) {
 	console.error( error );
 } );
-
+}
 
 
 //animating the scene    
 function animate() {
     requestAnimationFrame( animate );
+    model.rotation.y += 0.0025;
     renderer.render( scene, camera );
 }
 animate();
