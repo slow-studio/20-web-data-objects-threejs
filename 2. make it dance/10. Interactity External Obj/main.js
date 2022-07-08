05574e6a1a7c61b790a3c4e919799f7c571e2575
UddiptaG <uddipta.gogoi@slow.studio>
2022-07-08 13:37:09 +0530

    added interactivity to external object

---
+++ 2. make it dance/10. Interactity External Obj/main.js
@@ -0,0 +1,236 @@
+console.log("external objecr script running")
+
+//creating the scene
+const scene=new THREE.Scene();
+const gui=new dat.GUI()
+
+//adding the objects to this array for interactivity
+var objects=[];
+
+//creating a group
+var gltfGrop=new THREE.Group();
+
+//creating the scene mesh
+var sceneMeshes = []
+// sceneMeshes=new THREE.Mesh
+
+const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();
+
+//adding a grid helper to the scene
+const gridHelper=new THREE.GridHelper(10,10,0x000000,0xffffff);
+// gridHelper.position.y =-0.5
+// scene.add(gridHelper);
+
+
+//object to call the external assets
+rgbeLoader=getRGBLoader('./images/img3_4k.hdr') 
+var gltfObj1=getGLTFLoader('./assets/roun.glb',0,-1,0,0xffcc00);        //here we are passing the obj location and positions as the parameter
+
+
+//adding elements to the scene
+
+//adding the spheres
+for(let i=0;i<150;i++){
+    console.log(i)
+    var color=Math.random() * 0xffffff;
+    var rad=Math.random()*0.3;
+    var object=getSphere(rad,32,16,color);
+
+    //setting position for the object in random across the screen
+    object.position.x = (Math.random()) * 7-4;
+    object.position.y = Math.random() * 3;
+    object.position.z = (Math.random()) * 4-2;
+    scene.add( object );
+    objects.push( object );
+}
+
+
+
+//creaitng parent container 1
+var parentContainer1=new THREE.Mesh();
+scene.add(parentContainer1);
+    for(let j=0;j<50;j++){
+        parentContainer1.add(objects[j])
+    }
+
+//creaitng parent container 2
+var parentContainer2=new THREE.Mesh();
+scene.add(parentContainer2);
+for(let j=30;j<100;j++){
+    parentContainer2.add(objects[j])
+}
+
+//creaitng parent container 3
+var parentContainer3=new THREE.Mesh();
+scene.add(parentContainer3);
+for(let j=60;j<150;j++){
+    parentContainer3.add(objects[j])
+}   
+
+//setting up perspective camera
+var camera=new THREE.PerspectiveCamera(
+    40,                                      	 			//camera FOV
+    window.innerWidth/window.innerHeight,    	 			//camera aspectRatio
+    1,                                     	 				//nearSight
+    1000                                     		 		//farSight
+);
+
+//set camera positions
+camera.position.set(0,3,10);    	 	//set the camera position on on x,y,z axes
+camera.lookAt(0,0,0)            	//makes the camera look at the center of the object
+
+//setting up the renderer
+const renderer=new THREE.WebGLRenderer({
+    alpha:true,
+    antialias:true,
+    depth: true
+});                     //creating an instance of the renderer
+
+renderer.shadowMap.enabled = true;                          //enabling shadow in render
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;           //adding shadow type as soft shadow
+renderer.setSize( window.innerWidth, window.innerHeight);   //setting up the size of the renderer
+renderer.setClearColor(new THREE.Color('#ffffff'),0.45)
+
+//used to appx appearence of hdr o ndevice monitor
+renderer.toneMapping=THREE.ACESFilmicToneMapping;
+
+//exposure level of tonemapping, def=1
+renderer.toneMappingExposure=0.6;
+
+//interpolating color gradients
+renderer.outputEncoding=THREE.sRGBEncoding;
+
+document.body.appendChild( renderer.domElement);
+
+
+//setting up orbit controls
+var Orbcontrols = new THREE.OrbitControls(camera,renderer.domElement);
+Orbcontrols.maxDistance=12;    //set max zoom(dolly) out distance for perspective camera, default=infinity
+// controls.minDistance=0.75
+//Orbcontrols.maxPolarAngle = Math.PI/2.1;     //prevent orbit controls from going below the ground
+Orbcontrols.enableDamping = true;   //damping 
+Orbcontrols.dampingFactor = 0.25;   //damping inertia
+
+//setting up drag controls to drag an object around the screen
+const dControls = new THREE.DragControls( objects, camera, renderer.domElement );
+dControls.addEventListener( 'drag', render );
+
+//disabling orbit controls when drag controls are started
+dControls.addEventListener('dragstart',function(event){
+	Orbcontrols.enabled=false;
+})
+dControls.addEventListener('dragend',function(event){
+	Orbcontrols.enabled=true;
+})
+
+//drag hoverON
+dControls.addEventListener("hoveron", function(event){
+    console.log(event.object)
+    event.object.scale.x*=1.2
+    event.object.scale.y*=1.2
+    event.object.scale.z*=1.2
+    event.object.material.opacity=0.5
+   // event.object.material.color.setHex( Math.random() * 0xffffff );
+})
+
+//drag hoverOFF
+dControls.addEventListener("hoveroff", function(event){
+    console.log(event.object)
+    event.object.scale.x/=1.2
+    event.object.scale.y/=1.2
+    event.object.scale.z/=1.2
+    event.object.material.opacity=1
+   // event.object.material.color.setHex( Math.random() * 0xffffff );
+})
+
+//declaring the functions
+
+//adding the RGBE Loader
+function getRGBLoader(assetLocation){
+    const RGBELoader=new THREE.RGBELoader();
+    RGBELoader.load(assetLocation,function(texture){
+        texture.mapping=THREE.EquirectangularReflectionMapping;
+        scene.background=texture;
+        scene.environment=texture;
+        });
+}
+
+//function to call the GLTF Loader----the obj location and positions are passed as parameters in the function call
+function getGLTFLoader(assetLocation,positionX,positionY,positionZ,clr){
+    const loader=new THREE.GLTFLoader();
+    
+    let modelGroup= new THREE.Group
+    let modelDragBox=new THREE.Mesh
+
+loader.load( assetLocation, function ( gltf ) {
+    model1=gltf.scene;
+
+    const newMaterial = new THREE.MeshStandardMaterial({
+                                    color: clr,
+                                    transparent: true,
+                                    opacity: 1
+                                   // wireframe: true,
+                                   // roughness: 0,
+                                   // emissive: 0x000000,                     
+                        });
+						model1.traverse((o) => {
+						if (o.isMesh) o.material = newMaterial;
+						}); 
+
+                        //model.wireframe=true    
+                        model1.castShadow = true;
+                        model1.traverse(function (node) {
+                          if (node.isMesh) {
+                            node.castShadow = true;
+                            node.receiveShadow = true;
+                          }
+                        });
+
+    model1.scale.x*=1.5
+    model1.scale.y*=1.5
+    model1.scale.z*=1.5
+    model1.position.set(positionX,positionY,positionZ);        
+   
+    scene.add(model1);
+    sceneMeshes.push(model1)
+    return model1; 
+	
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+}
+
+//function to get a sphere-----------------------------
+function getSphere(radius,widthSegment,heightSegment,color){
+    const geometry=new THREE.SphereBufferGeometry(radius,widthSegment,heightSegment);
+    const material=new THREE.MeshStandardMaterial({      
+        color: color,
+        metalness:0.1,
+        roughness:0.5,
+        transparent: true,
+        opacity:1
+    });
+    const mesh=new THREE.Mesh(geometry,material);
+    mesh.receiveShadow=true;
+    mesh.castShadow=true;    
+    return mesh;
+}
+
+
+//function to animate the scene------
+animate();
+function animate() {   
+   // TWEEN.update();
+    requestAnimationFrame( animate );
+   //  model1.rotation.y += 0.0025;
+
+     parentContainer1.rotation.x+=0.0025;
+     parentContainer2.rotation.y+=0.0025;
+     parentContainer3.rotation.z+=0.0025;
+     
+   // camera.lookAt( scene.position );
+    render();
+}
+function render() {
+    renderer.render( scene, camera );
+}
--- 2. make it dance/9. Interactivty movement using movement/main.js
+++ 2. make it dance/9. Interactivty movement using movement/main.js
@@ -1,289 +1,271 @@
-// console.log("script running")
+console.log("script running")
 
-// const scene=new THREE.Scene();
-// const gui=new dat.GUI()
+const scene=new THREE.Scene();
+const gui=new dat.GUI()
 
-// var objects=[];
+var objects=[];         //all the objects are stored here to make them interactive
+const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();       //adding raycaster to control the objects using the mouse
 
-// //calling objects to initiate the elements
-// hemiLight=getHemiLight(0.5);
-// sphere1=getSphere(0.35,32,16,0x3290FF); 
-// sphere2=getSphere(0.2,32,16,0x3290FF); 
 
-// //adding elements to the scene
-// scene.add(hemiLight);
-// scene.add(sphere1);
-// scene.add(sphere2);
+//calling objects to initiate the elements
+rgbeLoader=getRGBLoader('./images/img_4k.hdr') 
+hemiLight=getHemiLight(0.5);
+spotLight=getSpotLight(0.5);
 
-// //adding elemnts to the objects array
-// objects.push(sphere1)
-// objects.push(sphere2)
+//adding elements to the scene
+scene.add(hemiLight);
+// scene.add(spotLight);
 
-// //setting up object positions
-// sphere1.position.y = sphere1.geometry.parameters.radius;      //setting up sphere position wrt y axis
-// sphere2.position.set(2,sphere2.geometry.parameters.radius,0)
+//adding the spheres
+for(let i=0;i<90;i++){
+		console.log(i)
+		var color=Math.random() * 0xffffff;
+		var rad=Math.random()*0.3;
+		var object=getSphere(rad,32,16,color);
+	
+        //setting position for the object in random across the screen
+		object.position.x = (Math.random()) * 7-5;
+		object.position.y = Math.random() * 3;
+		object.position.z = (Math.random()) * 4-2;
 
-// //creating a grid
+        scene.add( object );
+		objects.push( object );
+	
+	}
+
+//creaitng parent container 1
+    var parentContainer1=new THREE.Mesh();
+    scene.add(parentContainer1);
+        for(let j=0;j<30;j++){
+            parentContainer1.add(objects[j])
+        }
+
+//creaitng parent container 2
+var parentContainer2=new THREE.Mesh();
+scene.add(parentContainer2);
+    for(let j=30;j<60;j++){
+        parentContainer2.add(objects[j])
+    }
+
+//creaitng parent container 3
+var parentContainer3=new THREE.Mesh();
+scene.add(parentContainer3);
+    for(let j=60;j<90;j++){
+        parentContainer3.add(objects[j])
+    }    
+
+     	
+// //setting up light positions
+spotLight.position.set(10,300,1000)
+
+//creating a grid
 // const gridHelper=new THREE.GridHelper(10,10,0x000000,0xffffff);
-// // gridHelper.position.y =-0.5
+// gridHelper.position.y =-0.5
 // scene.add(gridHelper);
 
-// //setting up ther perspective camera
-// var camera=new THREE.PerspectiveCamera(
-//     50,                                       //camera FOV
-//     window.innerWidth/window.innerHeight,     //camera aspectRatio
-//     0.1,                                      //nearSight
-//     1000                                      //farSight
-// );
+//setting up ther perspective camera
+var camera=new THREE.PerspectiveCamera(
+    50,                                      	 			//camera FOV
+    window.innerWidth/window.innerHeight,    	 			//camera aspectRatio
+    1,                                     	 				//nearSight
+    1000                                     		 		//farSight
+);
 
-// //set camera positions
-// camera.position.set(0,1,4);     //set the camera position on on x,y,z axes
-// //camera.lookAt(0,0,0)            //makes the camera look at the center of the object
+//set camera positions
+camera.position.set(0,1,10);    	 	//set the camera position on on x,y,z axes
+camera.lookAt(0,0,0)            	//makes the camera look at the center of the object
 
-// //setting up the renderer
-// const renderer=new THREE.WebGLRenderer({
-//     alpha:true,
-//     antialias:true,
-//     depth: true
-// });                     //creating an instance of the renderer
+//setting up the renderer
+const renderer=new THREE.WebGLRenderer({
+    alpha:true,
+    antialias:true,
+    depth: true
+});                     //creating an instance of the renderer
 
-// renderer.shadowMap.enabled = true;                          //enabling shadow in render
-// renderer.shadowMap.type = THREE.PCFSoftShadowMap;           //adding shadow type as soft shadow
-// renderer.setSize( window.innerWidth, window.innerHeight);   //setting up the size of the renderer
-// renderer.setClearColor(new THREE.Color('#ffffff'),0.45)
-// document.body.appendChild( renderer.domElement);
+renderer.shadowMap.enabled = true;                          //enabling shadow in render
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;           //adding shadow type as soft shadow
+renderer.setSize( window.innerWidth, window.innerHeight);   //setting up the size of the renderer
+renderer.setClearColor(new THREE.Color('#ffffff'),0.45)
 
-// //setting up orbit controls
-// controls = new THREE.OrbitControls(camera,renderer.domElement);
-// // controls.maxDistance=12;    //set max zoom(dolly) out distance for perspective camera, default=infinity
-// // controls.minDistance=0.75
-// controls.maxPolarAngle = Math.PI/2.1;     //prevent orbit controls from going below the ground
-// controls.enableDamping = true;   //damping 
-// controls.dampingFactor = 0.25;   //damping inertia
+//used to appx appearence of hdr o ndevice monitor
+renderer.toneMapping=THREE.ACESFilmicToneMapping;
 
-// //function to add a hemi light-------------------------
-// function getHemiLight(intensity){
-//     const light=new THREE.HemisphereLight(0xffffee,0xffffee, intensity)
-//     return light;
-// }
+//exposure level of tonemapping, def=1
+renderer.toneMappingExposure=0.6;
 
-// //function to get a sphere-----------------------------
-// function getSphere(radius,widthSegment,heightSegment,color){
-//     const geometry=new THREE.SphereBufferGeometry(radius,widthSegment,heightSegment);
-//     const material=new THREE.MeshBasicMaterial({      
-//         color: color,
-//         metalness:0.1,
-//         roughness:0.5,
-//         transparent: true,
-//         opacity:1
-//     });
-//     const mesh=new THREE.Mesh(geometry,material);
-//     mesh.receiveShadow=true;
-//     mesh.castShadow=true;    
-//     return mesh;
-// }
+//interpolating color gradients
+renderer.outputEncoding=THREE.sRGBEncoding;
 
-// //calling the mousedown through addEventListener
-// document.addEventListener('mousedown',onDocumentMouseDown);
+document.body.appendChild( renderer.domElement);
 
-// //defining the mouseDown function
-// function onDocumentMouseDown(event){
-//     event.preventDefault();
-//     var mouse3D = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1,   
-//                                     -( event.clientY / window.innerHeight ) * 2 + 1,  
-//                                     0.5 );   
-//     var raycaster=new THREE.Raycaster();
-//     raycaster.setFromCamera(mouse3D,camera);
+//setting up orbit controls
+var Orbcontrols = new THREE.OrbitControls(camera,renderer.domElement);
+Orbcontrols.maxDistance=12;                 //set max zoom(dolly) out distance for perspective camera, default=infinity
+Orbcontrols.minDistance=3
+Orbcontrols.maxPolarAngle = Math.PI/2.1;    //prevent orbit controls from going below the ground
+Orbcontrols.enableDamping = true;           //damping 
+Orbcontrols.dampingFactor = 0.25;           //damping inertia
+
+//setting up drag controls to drag an object around the screen
+const dControls = new THREE.DragControls( objects, camera, renderer.domElement );
+dControls.addEventListener( 'drag', render );
+
+//disabling orbit controls when drag controls are started
+dControls.addEventListener('dragstart',function(event){
+	Orbcontrols.enabled=false;
+})
+dControls.addEventListener('dragend',function(event){
+	Orbcontrols.enabled=true;
+})
+
+//adding eventListeners
+//document.addEventListener( 'click', onClick );
+
+//drag hoverON
+dControls.addEventListener("hoveron", function(event){
+    console.log(event.object)
+    event.object.scale.x*=2
+    event.object.scale.y*=2
+    event.object.scale.z*=2
+    event.object.material.color.setHex( Math.random() * 0xffffff );
+})
+
+//drag hoverOFF
+dControls.addEventListener("hoveroff", function(event){
+    console.log(event.object)
+    event.object.scale.x/=2
+    event.object.scale.y/=2
+    event.object.scale.z/=2
+    event.object.material.color.setHex( Math.random() * 0xffffff );
+})
+
+//drag Start
+dControls.addEventListener("dragstart", function(event){
+    console.log(event.object)
+  
+    event.object.material.opacity=0.5;
+    //event.object.material.opacity=1;
+})
+
+dControls.addEventListener("dragend", function(event){
+    console.log(event.object)
+  
+    event.object.material.opacity=1;
+    //event.object.material.opacity=1;
+})
+
+window.addEventListener( 'keydown', onKeyDown );
+window.addEventListener( 'keyup', onKeyUp );
+
+
+//function to add a hemi light-------------------------
+function getHemiLight(intensity){
+    const light=new THREE.HemisphereLight(0xffffee,0xffffee, intensity)
+    return light;
+}
+
+//function to add a spotlight
+function getSpotLight(intensity){
+    const light=new THREE.SpotLight(0xffffff,intensity);
+    light.castShadow=true;
+    light.shadow.bias= -0.00001;
+    light.shadow.mapSize.width=1024*4;
+    light.shadow.mapSize.height=1024*4;
+    return light;
+}
+
+//function to get a sphere-----------------------------
+function getSphere(radius,widthSegment,heightSegment,color){
+    const geometry=new THREE.SphereBufferGeometry(radius,widthSegment,heightSegment);
+    const material=new THREE.MeshStandardMaterial({      
+        color: color,
+        metalness:0.1,
+        roughness:0.5,
+        transparent: true,
+        opacity:1
+    });
+    const mesh=new THREE.Mesh(geometry,material);
+    mesh.receiveShadow=true;
+    mesh.castShadow=true;    
+    return mesh;
+}
+
+
+//defining the mouseDown function
+function onClick(event){
+    console.log("intitate on click")
+    console.log(event)
+	
+    event.preventDefault();
+    var mouse3D = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1,   
+                                    -( event.clientY / window.innerHeight ) * 2 + 1,  
+                                    0.5 );   
+    var raycaster=new THREE.Raycaster();
+    raycaster.setFromCamera(mouse3D,camera);
     
-//     var intersects=raycaster.intersectObjects(objects);
-//     console.log(intersects)
+    var intersects=raycaster.intersectObjects(objects);
+    
+    console.log(intersects)
 
-//     if(intersects.length>0){
-//         intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
-//     }
+    if(intersects.length>0){
+        
+        intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+    }
+	// render();
+}
+
+// function onClick(event){
+//     console.log(event.object);
 // }
 
-// //function to animate the scene------
-// animate();
-// function animate() {   
-//    // TWEEN.update();
-//     requestAnimationFrame( animate );
-//    // camera.lookAt( scene.position );
-//     render();
-// }
-// function render() {
-//     renderer.render( scene, camera );
-// }
-
-
-//-------------------------
-
-
-let container;
-			let camera, scene, renderer;
-			let controls, group;
-			let enableSelection = false;
-
-			const objects = [];
-
-			const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();
-
-			init();
-
-			function init() {
-
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
-
-				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 5000 );
-				camera.position.z = 1000;
-
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0xf0f0f0 );
-
-				scene.add( new THREE.AmbientLight( 0x505050 ) );
-
-				const light = new THREE.SpotLight( 0xffffff, 1.5 );
-				light.position.set( 0, 500, 2000 );
-				light.angle = Math.PI / 9;
-
-				light.castShadow = true;
-				light.shadow.camera.near = 1000;
-				light.shadow.camera.far = 4000;
-				light.shadow.mapSize.width = 1024;
-				light.shadow.mapSize.height = 1024;
-
-				scene.add( light );
-
-				group = new THREE.Group();
-				scene.add( group );
-
-				const geometry = new THREE.BoxGeometry( 40, 40, 40 );
-
-				for ( let i = 0; i < 200; i ++ ) {
-
-					const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-
-					object.position.x = Math.random() * 1000 - 500;
-					object.position.y = Math.random() * 600 - 300;
-					object.position.z = Math.random() * 800 - 400;
-
-					object.rotation.x = Math.random() * 2 * Math.PI;
-					object.rotation.y = Math.random() * 2 * Math.PI;
-					object.rotation.z = Math.random() * 2 * Math.PI;
-
-					object.scale.x = Math.random() * 2 + 1;
-					object.scale.y = Math.random() * 2 + 1;
-					object.scale.z = Math.random() * 2 + 1;
-
-					object.castShadow = true;
-					object.receiveShadow = true;
-
-					scene.add( object );
-
-					objects.push( object );
-
-				}
-
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-				renderer.shadowMap.enabled = true;
-				renderer.shadowMap.type = THREE.PCFShadowMap;
-
-				container.appendChild( renderer.domElement );
-
-				controls = new DragControls( [ ... objects ], camera, renderer.domElement );
-				controls.addEventListener( 'drag', render );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize );
-
-				document.addEventListener( 'click', onClick );
-				window.addEventListener( 'keydown', onKeyDown );
-				window.addEventListener( 'keyup', onKeyUp );
-
-				render();
-
-			}
-
-			function onWindowResize() {
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-				render();
-
-			}
-
-			function onKeyDown( event ) {
-
-				enableSelection = ( event.keyCode === 16 ) ? true : false;
-
-			}
-
-			function onKeyUp() {
-
-				enableSelection = false;
-
-			}
-
-			function onClick( event ) {
-
-				event.preventDefault();
-
-				if ( enableSelection === true ) {
-
-					const draggableObjects = controls.getObjects();
-					draggableObjects.length = 0;
-
-					mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-					mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-					raycaster.setFromCamera( mouse, camera );
-
-					const intersections = raycaster.intersectObjects( objects, true );
-
-					if ( intersections.length > 0 ) {
-
-						const object = intersections[ 0 ].object;
-
-						if ( group.children.includes( object ) === true ) {
-
-							object.material.emissive.set( 0x000000 );
-							scene.attach( object );
-
-						} else {
-
-							object.material.emissive.set( 0xaaaaaa );
-							group.attach( object );
-
-						}
-
-						controls.transformGroup = true;
-						draggableObjects.push( group );
-
-					}
-
-					if ( group.children.length === 0 ) {
-
-						controls.transformGroup = false;
-						draggableObjects.push( ...objects );
-
-					}
-
-				}
-
-				render();
-
-			}
-
-			function render() {
-
-				renderer.render( scene, camera );
-
-			}
+//function onKeyDown
+function onKeyDown( event ) {
+	enableSelection = ( event.keyCode === 16 ) ? true : false;
+}
+
+//function onKeyUp
+function onKeyUp() {
+	enableSelection = false;
+}
+
+
+
+//adding windows resize functionalities-------------
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    // render();
+}
+
+//adding the RGBE Loader
+function getRGBLoader(assetLocation){
+    const RGBELoader=new THREE.RGBELoader();
+    RGBELoader.load(assetLocation,function(texture){
+        texture.mapping=THREE.EquirectangularReflectionMapping;
+        scene.background=texture;
+        scene.environment=texture;
+        });
+}
+
+//function to animate the scene------
+animate();
+function animate() {   
+   // TWEEN.update();
+    requestAnimationFrame( animate );
+
+  
+    parentContainer1.rotation.x+=0.0025;
+    parentContainer2.rotation.y+=0.0025;
+    parentContainer3.rotation.z+=0.0025;
+    
+ 
+
+   // camera.lookAt( scene.position );
+    render();
+}
+function render() {
+    renderer.render( scene, camera );
+}
