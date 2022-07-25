46c69b53317dc824920ffe664bfbbdd721cb94f8
UddiptaG <uddipta.gogoi@slow.studio>
2022-07-25 18:29:43 +0530

    added a scale and position transition to the objects

--- 6. Income Disparity/1. transition/main.js
+++ 6. Income Disparity/1. transition/main.js
@@ -1,10 +1,11 @@
 console.log("initiating income disparity")
 
 let objects=[]  //we will store all the objects in this array once they are initiated
+var shouldTransition;
 
 /*---------set camera variables--------------------*/
 //set the camera parameters
-var camera_FOV=45, cameraAspectRatio=(window.innerWidth/window.innerHeight),camera_NearSight=1,camera_FarSight=30000
+var camera_FOV=35, cameraAspectRatio=(window.innerWidth/window.innerHeight),camera_NearSight=1,camera_FarSight=30000
 //declaring camera position
 cameraPosition_x=0, cameraPosition_y=2,cameraPosition_z=30;
 //declaring camera lookat
@@ -34,12 +35,22 @@ let numberOfColoumns=4;         //total number of coloumns
 const scene=new THREE.Scene();
 
 
-/*--adding elements to the scene-------------*/
+/*----------adding a grid helper---------*/
+
+const size = 50;
+const divisions = 10;
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper )
+
+
+/*------adding elements to the scene-------------*/
 //adding the lights
 scene.add(ambientLight)
 
 //adding the spheres to the scene
 getMultipleSpheres(sphereWidthSegmets, sphereHeightSegmets,populationWealthDistribution)
+
+
 
 
 /*----adding a perspective camera to the scene-------------------------------------------*/
@@ -78,13 +89,35 @@ Orbcontrols.enableDamping = true;   //damping
 Orbcontrols.dampingFactor = 0.25;   //damping inertia
 
 
+//setting up drag controls to drag an object around the screen
+const dControls = new THREE.DragControls( objects, camera, renderer.domElement );
+dControls.addEventListener( 'drag', render );
+
+//disabling orbit controls when drag controls are started
+dControls.addEventListener('dragstart',function(event){
+	Orbcontrols.enabled=false;
+})
+dControls.addEventListener('dragend',function(event){
+	Orbcontrols.enabled=true;
+})
+
+dControls.addEventListener("hoveron", function(event){
+	event.object.material.opacity=0.5;
+})
+
+dControls.addEventListener("hoveroff", function(event){
+	event.object.material.opacity=1
+})
+
+
 //function to add genreate multiple speheres
 function getMultipleSpheres(sphereWidthSegmets, sphereHeightSegmets,populationWealthDistribution){
     var sphereTotal=populationWealthDistribution.length;
     
     //declaring a for loop to add the multiple spheres
     for(let i=0;i<sphereTotal;i++){
-        var sphereColor=populationWealthDistribution[i].color;
+        // var sphereColor=populationWealthDistribution[i].color;
+		var sphereColor=Math.random() * 0xffffff;
 		var sphereRadius=populationWealthDistribution[i].radius;
 
         console.log("sphere total "+sphereRadius)
@@ -152,6 +185,10 @@ function getSphere(radius,widthSegmets,heightSegmets,sphereColor){
     return mesh;
 }
 
+
+
+
+
 /*----declaring Event Listeners------------*/
 //adding event listener for the CTA button
 document.getElementById("start").addEventListener("click", transitionStartonClick);
@@ -160,14 +197,84 @@ document.getElementById("start").addEventListener("click", transitionStartonClic
 window.addEventListener( 'resize', onWindowResize );  
 
 
-/*-----defining function for transition on click------*/
+/*-----defining the add EventListener functions----*/
+
 function transitionStartonClick(){
-    
-    // ambientLight.visible = !ambientLight.visible;
-    console.log(objects)
-    
+	
+	transitionByCameraPosition(0,6,65);
+	transitionByScale()  
+	transitionByPosition()
+	// document.getElementById("start").style.visibility="hidden";       
 }
 
+//declaring transition by change in camera position
+function transitionByCameraPosition(changeCameraPosition_x,changeCameraPosition_y,changeCameraPosition_z){
+	camera.position.x=changeCameraPosition_x
+	camera.position.y=changeCameraPosition_y
+	camera.position.z=changeCameraPosition_z
+}
+
+//declaring transition by scale
+function transitionByScale(){
+	console.log("transition by scale")
+	for(let i=0;i<objects.length;i++){
+		
+		var wealthDist=populationWealthDistribution[i].wealthDistribution;
+		// console.log(wealthDist)
+		if(wealthDist<1){
+			// console.log("object "+(i+1)+" populaiton distribution: "+wealthDist)				
+			var scaleAmt=wealthDist*1.5;
+			objects[i].scale.x=scaleAmt;
+			objects[i].scale.y=scaleAmt;
+			objects[i].scale.z=scaleAmt;
+
+		}else if(wealthDist>1 && wealthDist<10){
+
+			if(wealthDist<5){
+				var scaleAmt=wealthDist*1;
+				objects[i].scale.x=scaleAmt;
+				objects[i].scale.y=scaleAmt;
+				objects[i].scale.z=scaleAmt;					
+			}else{
+				var scaleAmt=wealthDist*1;
+				objects[i].scale.x=scaleAmt;
+				objects[i].scale.y=scaleAmt;
+				objects[i].scale.z=scaleAmt;				
+			}
+
+		}else{
+
+			if(wealthDist<50){
+				var scaleAmt=wealthDist*1;
+				objects[i].scale.x=scaleAmt;
+				objects[i].scale.y=scaleAmt;
+				objects[i].scale.z=scaleAmt;
+			}else{
+				var scaleAmt=wealthDist*0.75;
+				objects[i].scale.x=scaleAmt;
+				objects[i].scale.y=scaleAmt;
+				objects[i].scale.z=scaleAmt;
+			}
+		}
+	}
+}
+
+
+//declaring transition by change in posiiton
+function transitionByPosition(){
+	
+	objects[0].position.set(-38,-11.5,0)			//sphere 5
+	objects[1].position.set(-35.5,-11.3,0)			//sphere 5
+	objects[2].position.set(-32,-10.7,0)			//sphere 5
+	objects[3].position.set(-28,-10.5,0)			//sphere 5
+	objects[4].position.set(-23,-10,0)				//sphere 5
+	objects[5].position.set(-16.5,-9,0)				//sphere 6
+	objects[6].position.set(-7.5,-7.5,0)			//sphere 7
+	objects[7].position.set(5,-5,0)					//sphere 8
+	objects[8].position.set(25,0,0)					//sphere 9
+	objects[9].position.set(75,3,-50)				//sphere 10
+
+}
 
 
 
---
+++ 6. Income Disparity/assets/guiControls.js
@@ -0,0 +1,71 @@
+const gui=new dat.GUI();
+dat.GUI.toggleHide();       //ensures the GUI displays hidden by default, press H to view it
+
+/*---adding the dat gui elements----*/
+//creating the folders
+const sphereGUI_1=gui.addFolder('sphere 1');
+const sphereGUI_2=gui.addFolder('sphere 2');
+const sphereGUI_3=gui.addFolder('sphere 3');
+const sphereGUI_4=gui.addFolder('sphere 4')
+const sphereGUI_5=gui.addFolder('sphere 5')
+const sphereGUI_6=gui.addFolder('sphere 6')
+const sphereGUI_7=gui.addFolder('sphere 7')
+const sphereGUI_8=gui.addFolder('sphere 8')
+const sphereGUI_9=gui.addFolder('sphere 9')
+const sphereGUI_10=gui.addFolder('sphere 10')
+const cameraControls=gui.addFolder('camera controls')
+
+//sphere 1 controls
+sphereGUI_1.add(objects[0].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_1.add(objects[0].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_1.add(objects[0].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 2 controls
+sphereGUI_2.add(objects[1].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_2.add(objects[1].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_2.add(objects[1].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 3 controls
+sphereGUI_3.add(objects[2].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_3.add(objects[2].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_3.add(objects[2].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 4 controls
+sphereGUI_4.add(objects[3].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_4.add(objects[3].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_4.add(objects[3].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 5 controls
+sphereGUI_5.add(objects[4].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_5.add(objects[4].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_5.add(objects[4].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 6 controls
+sphereGUI_6.add(objects[5].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_6.add(objects[5].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_6.add(objects[5].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 7 controls
+sphereGUI_7.add(objects[6].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_7.add(objects[6].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_7.add(objects[6].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 8 controls
+sphereGUI_8.add(objects[7].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_8.add(objects[7].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_8.add(objects[7].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 9 controls
+sphereGUI_9.add(objects[8].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_9.add(objects[8].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_9.add(objects[8].position, 'z').min(-100).max(100).step(0.01);
+
+//sphere 10 controls
+sphereGUI_10.add(objects[9].position, 'x').min(-100).max(100).step(0.01);
+sphereGUI_10.add(objects[9].position, 'y').min(-100).max(100).step(0.01);
+sphereGUI_10.add(objects[9].position, 'z').min(-100).max(100).step(0.01);
+
+//camera controls
+cameraControls.add(camera.position, 'x').min(-100).max(100).step(0.01);
+cameraControls.add(camera.position, 'y').min(-100).max(100).step(0.01);
+cameraControls.add(camera.position, 'z').min(-100).max(500).step(0.01);
